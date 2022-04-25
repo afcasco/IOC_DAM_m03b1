7681adc4259b4561da0aa76458f10e581db63fd5
afcasco <afcasco@gmail.com>
2022-04-25 12:36:27 +0200

    Update Exercici_10.java

--- Exercici_10.java
+++ Exercici_10.java
@@ -13,53 +13,54 @@ public class E10_FernandezC {
     public static void main(String[] args) {
 
         //variables declaration
-        Scanner istream = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         boolean inRange;
         int limit = 0;
         int repeat = 0;
         int cumulative;
+        int totalOdds;
 
         //run until repeat reaches N_LOOPS
         do {
             //assign inside the loop to avoid carry-over between runs
             inRange = true;
             cumulative = 0;
-            System.out.println("\n----------------------------------");
-            System.out.println("Here we go! LOOP: " + (repeat + 1) + "/" + N_LOOPS);
-            System.out.println("----------------------------------\n");
+            totalOdds = 0;
+            System.out.println("\n---------------------------------------");
+            System.out.println("| Here we go! LOOP: " + (repeat + 1) + "/" + N_LOOPS + "               |");
+            System.out.println("---------------------------------------\n");
 
             //run while conditions are not met (exit with a valid number or -1)
             do {
                 //Will only show the error message after the first run
-                if (!inRange) System.out.println("Enter a number in the specified range please...");
-                System.out.println("Enter integer between 20 and 100 (-1 to exit): ");
-                inRange = istream.hasNextInt();
+                if (!inRange) System.out.println("Invalid input, stick to the rules please!");
+                System.out.println("Enter an integer between 20 and 100 (-1 to exit): ");
+                inRange = input.hasNextInt();
                 if (inRange) {
-                    limit = istream.nextInt();
-                    if (limit < NUM_MIN || limit > NUM_MAX) {
-                        inRange = false;
-                    }
+                    limit = input.nextInt();
+                    if (limit < NUM_MIN || limit > NUM_MAX) inRange = false;
                 }
-                istream.nextLine();
+                input.nextLine();
             } while (!inRange && limit != EXIT);
 
             //If loop was ended with valid number (not -1)
             if (inRange) {
-                System.out.println("Odds in given range:");
+                System.out.println("Odd numbers in the given range:");
                 for (int i = 0; i <= limit; i++) {
+                    //Only print odd numbers and increment totalOdds
                     if (i % 2 != 0) {
                         System.out.print(i);
-                        //Avoid trailing comma
+                        totalOdds++;
+                        //Avoid trailing comma and print how many odds were found
                         if (i < limit - 1) System.out.print(", ");
+                        else System.out.print("  (" + totalOdds + " found!)");
                         cumulative += i;
                     }
                 }
-                System.out.println("\nAll added odds in range: " + cumulative);
+                System.out.println("\nThe sum of all the odd numbers in the series is: " + cumulative);
 
                 //If we exit the loop with -1 print error message
-            } else {
-                System.out.println("EXIT TRIGGERED BY USER");
-            }
+            } else System.out.println("\nEXIT TRIGGERED BY USER");
 
             repeat++;
         } while (repeat < N_LOOPS);
