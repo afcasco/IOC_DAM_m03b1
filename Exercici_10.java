8c18ba5b6b36b36a224a56094b7680213b7b164a
afcasco <afcasco@gmail.com>
2022-04-25 11:54:24 +0200

    Update Exercici_10.java

--- Exercici_10.java
+++ Exercici_10.java
@@ -1,57 +1,67 @@
+package Unitat1;
+
 import java.util.Scanner;
 
-public class Exercici10 {
+public class E10_FernandezC {
 
-    private static final int NUM_MIN =  20;
+    //Declaracio de constants
+    private static final int NUM_MIN = 20;
     private static final int NUM_MAX = 100;
     private static final int EXIT = -1;
-    private static final int MAX_LOOPS = 2;
-    
+    private static final int N_LOOPS = 2;
+
     public static void main(String[] args) {
 
+        //Declaracio de variables
         Scanner istream = new Scanner(System.in);
-        boolean inRange = true;
-        int limit=0;
-        int cumulative=0;
+        boolean inRange;
+        int limit = 0;
         int repeat = 0;
+        int cumulative;
 
-        do{
-            System.out.println("\n----------------------------------"); 
-            System.out.println("Here we go! LOOP: "+(repeat+1)+"/"+MAX_LOOPS);
-            System.out.println("----------------------------------\n"); 
+        //run until repeat reaches N_LOOPS
+        do {
+            //assing inside the loop to avoid carry-over between runs
+            inRange = true;
+            cumulative = 0;
+            System.out.println("\n----------------------------------");
+            System.out.println("Here we go! LOOP: " + (repeat + 1) + "/" + N_LOOPS);
+            System.out.println("----------------------------------\n");
 
+            //run while conditions are not met (exit with a valid number or -1)
             do {
-
-                if(!inRange) System.out.println("Enter a number in the specified range please...");
-
-                System.out.println("Enter integer between 20 and 100: ");
+                //Will only show the error message after the first run
+                if (!inRange) System.out.println("Enter a number in the specified range please...");
+                System.out.println("Enter integer between 20 and 100 (-1 to exit): ");
                 inRange = istream.hasNextInt();
                 if (inRange) {
-
                     limit = istream.nextInt();
                     if (limit < NUM_MIN || limit > NUM_MAX) {
                         inRange = false;
-
                     }
                 }
                 istream.nextLine();
             } while (!inRange && limit != EXIT);
 
+            //If we got a valid number run this
             if (inRange) {
-                for(int i=0; i <= limit; i++) {
-                    if(i%2!=0) {
-                        System.out.println("odd number: "+i);
+                System.out.println("Odds in given range:");
+                for (int i = 0; i <= limit; i++) {
+                    if (i % 2 != 0) {
+                        System.out.print(i);
+                        //Avoid trailing comma
+                        if (i < limit - 1) System.out.print(", ");
                         cumulative += i;
                     }
                 }
-                System.out.println("All added odds in range: "+cumulative);
-            } else {
+                System.out.println("\nAll added odds in range: " + cumulative);
 
+                //If we exit with -1 print error message
+            } else {
                 System.out.println("EXIT TRIGGERED BY USER");
             }
 
-            repeat++;    
-
-        } while(repeat<MAX_LOOPS); 
+            repeat++;
+        } while (repeat < N_LOOPS);
     }
 }
