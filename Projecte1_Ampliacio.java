ff18b7c52bb8eb0e1c7c33c448b634a81972aaf1
afcasco <afcasco@gmail.com>
2022-04-25 23:06:42 +0200

    Update Projecte1_Ampliacio.java

--- Projecte1_Ampliacio.java
+++ Projecte1_Ampliacio.java
@@ -14,7 +14,7 @@ public class AmpliacioP1 {
     private static final int TELF_MAX = 999999999;
     private static final int SI = 1;
     private static final int NO = 0;
-    private static final int TRIES_MAX = 3;
+    private static final int MAX_ATTEMPTS = 3;
     private static final String SHARED = "Habitació compartida             ";
     private static final String SINGLE = "Habitació                        ";
     private static final String HOME = "Habitatge sencer                 ";
@@ -28,7 +28,7 @@ public class AmpliacioP1 {
         Scanner input = new Scanner(System.in);
 
         //Declarem variables
-        int id, places, respostaIdioma, room, telf, tries, nFamilies =0;
+        int id, places, respostaIdioma, room, telf, attempts, nFamilies =0;
         String parlaIdioma, allotjament;
         boolean inRange;
         boolean keepAsking;
@@ -42,7 +42,7 @@ public class AmpliacioP1 {
             respostaIdioma = 0;
             room = 0;
             telf = 0;
-            tries = 0;
+            attempts = 0;
             parlaIdioma = PARLA_NO;
             inRange = false;
             //Mostrem a l'usuari com s'han d'introduir les dades
@@ -53,67 +53,68 @@ public class AmpliacioP1 {
             System.out.println("********************************************************************\n");
 
             //Demanem que s'introdueixi una dada fins que el valor introduit esta dins el rang acceptat
-            while (!inRange && tries < TRIES_MAX) {
+            while (!inRange && attempts < MAX_ATTEMPTS) {
                 System.out.println("Introdueix id familia (10-999):");
                 inRange = input.hasNextInt();
                 if (inRange) {
                     id = input.nextInt();
                     if (id < ID_MIN || id > ID_MAX) {
                         inRange = false;
-                        tries++;
                     }
                 }
-                if (!inRange) System.out.println(ERROR_MSG);
+                if (!inRange) {
+                    System.out.println(ERROR_MSG);
+                    attempts++;
+                }
                 input.nextLine();
             }
 
-            if (tries == TRIES_MAX) {
-                inRange = true;
-            } else {
-                inRange = false;
-                tries = 0;
+            if (attempts != MAX_ATTEMPTS) {
+                inRange=false;
+                attempts=0;
             }
-
             //Demanem que s'introdueixi una dada fins que el valor introduit esta dins el rang acceptat
-            while (!inRange && tries < TRIES_MAX) {
+            while (!inRange && attempts < MAX_ATTEMPTS) {
                 System.out.println("Introdueix número de places disponible (1-99):");
                 inRange = input.hasNextInt();
                 if (inRange) {
                     places = input.nextInt();
                     if (places < PLACES_MIN || places > PLACES_MAX) {
                         inRange = false;
-                        tries++;
                     }
                 }
-                if (!inRange) System.out.println(ERROR_MSG);
+                if (!inRange) {
+                    System.out.println(ERROR_MSG);
+                    attempts++;
+                }
                 input.nextLine();
             }
-            if (tries == TRIES_MAX) {
-                inRange = true;
-            } else {
-                inRange = false;
-                tries = 0;
+
+            if (attempts != MAX_ATTEMPTS) {
+                inRange=false;
+                attempts=0;
             }
 
             //Demanem que s'introdueixi una dada fins que el valor introduit esta dins el rang acceptat
-            while (!inRange && tries < TRIES_MAX) {
+            while (!inRange && attempts < MAX_ATTEMPTS) {
                 System.out.println("Parla rus o ucraines? (0=no, 1=si):");
                 inRange = input.hasNextInt();
                 if (inRange) {
                     respostaIdioma = input.nextInt();
                     if (respostaIdioma < NO || respostaIdioma > SI) {
                         inRange = false;
-                        tries++;
                     }
                 }
-                if (!inRange) System.out.println(ERROR_MSG);
+                if (!inRange){
+                    System.out.println(ERROR_MSG);
+                    attempts++;
+                }
                 input.nextLine();
             }
-            if (tries == TRIES_MAX) {
-                inRange = true;
-            } else {
-                inRange = false;
-                tries = 0;
+
+            if (attempts != MAX_ATTEMPTS) {
+                inRange=false;
+                attempts=0;
                 //Mostra informacio relativa al tipus d'allotjament a l'usuari
                 System.out.println("De quin tipus d'allotjament es tracta?:");
                 System.out.println("      (0) habitació compartida:");
@@ -123,42 +124,46 @@ public class AmpliacioP1 {
             }
 
             //Demanem que s'introdueixi una dada fins que el valor introduit esta dins el rang acceptat
-            while (!inRange && tries < TRIES_MAX) {
+            while (!inRange && attempts < MAX_ATTEMPTS) {
                 System.out.println("Introdueix una de les opcions:");
                 inRange = input.hasNextInt();
                 if (inRange) {
                     room = input.nextInt();
                     if (room < ROOM_MIN || room > ROOM_MAX) {
                         inRange = false;
-                        tries++;
+
                     }
                 }
-                if (!inRange) System.out.println(ERROR_MSG);
+                if (!inRange) {
+                    System.out.println(ERROR_MSG);
+                    attempts++;
+                }
                 input.nextLine();
             }
-            if (tries == TRIES_MAX) {
-                inRange = true;
-            } else {
-                inRange = false;
-                tries = 0;
+
+            if (attempts != MAX_ATTEMPTS) {
+                inRange=false;
+                attempts=0;
             }
 
             //Demanem que s'introdueixi una dada fins que el valor introduit esta dins el rang acceptat
-            while (!inRange && tries < TRIES_MAX) {
+            while (!inRange && attempts < MAX_ATTEMPTS) {
                 System.out.println("Introdueix un telefon de contacte:");
                 inRange = input.hasNextInt();
                 if (inRange) {
                     telf = input.nextInt();
                     if (telf < TELF_MIN || telf > TELF_MAX) {
                         inRange = false;
-                        tries++;
                     }
                 }
-                if (!inRange) System.out.println(ERROR_MSG);
+                if (!inRange){
+                    System.out.println(ERROR_MSG);
+                    attempts++;
+                }
                 input.nextLine();
             }
 
-            if (tries != TRIES_MAX) {
+            if (attempts != MAX_ATTEMPTS) {
             /*
             Fem les seguents operacions fora dels loops per facilitar la lectura del programa
             hem inicialitzat parlaidioma a "no" nomes la cambiem a "si" si es respon 1 a la pregunta
@@ -195,22 +200,24 @@ public class AmpliacioP1 {
             }
 
             inRange = false;
-            tries = 0;
-            while (!inRange && tries < TRIES_MAX) {
+            attempts = 0;
+            while (!inRange && attempts < MAX_ATTEMPTS) {
                 System.out.println("\nContinuar introduint dades? 0=no, 1=si");
                 inRange = input.hasNextInt();
                 if (inRange) {
                     keepAskingInt = input.nextInt();
                     if (keepAskingInt < NO || keepAskingInt > SI) {
                         inRange = false;
-                        tries++;
                     }
                 }
-                if (!inRange) System.out.println(ERROR_MSG);
+                if (!inRange){
+                    System.out.println(ERROR_MSG);
+                    attempts++;
+                }
                 input.nextLine();
             }
 
-            if (keepAskingInt == 0 || tries == 3) {
+            if (keepAskingInt == 0 || attempts == MAX_ATTEMPTS) {
                 keepAsking = false;
             }
         } while (keepAsking);
