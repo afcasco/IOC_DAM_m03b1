3ce7c322080d384e6d365f5ab003500e228bf924
afcasco <afcasco@gmail.com>
2022-04-27 19:51:59 +0200

    Update Exercici_12.java

--- Exercici_12.java
+++ Exercici_12.java
@@ -1,44 +1,60 @@
-import java.util.Scanner;
+	import java.util.Scanner;
 
-public class Arrays {
-	public static void main(String[] args) {
-		
-		//variables
-		int people;
-		Scanner input = new Scanner(System.in);
+	public class Arrays {
 
-		//ask for array size and declare array
-		System.out.println("Quantes persones vols entrar: ");
-		people = input.nextInt();
-		int[] userCode = new int[people];
+		private static final int NUM_MIN = 0;
+		private static final int NUM_MAX = 1000;
 
-		//Collect data into array
-		for (int i=0;i<people;i++) {
-			System.out.println("Entra el codi de l'usuari: ");
-			userCode[i]= input.nextInt();	
-		}
+		public static void main(String[] args) {
+			
+			//variables
+			int people;
+			boolean inRange;
+			Scanner input = new Scanner(System.in);
 
-		//Show all array values	
-		System.out.println("\nLlistat de codis entrats: ");
-		for (int i=0;i<people;i++) {
-		    System.out.println(userCode[i]);
-		}
+			//ask for array size and declare array
+			System.out.println("Quantes persones vols entrar: ");
+			people = input.nextInt();
+			int[] userCode = new int[people];
 
-		//bubble sort array values
-   		 for (int i = 0; i < people - 1; i++) {
-      		for (int j = 0; j < people - i - 1; j++) {
-       			 if (userCode[j] > userCode[j + 1]) {
-         		 	int aux = userCode[j];
-        		 	userCode[j] = userCode[j + 1];
-        		 	userCode[j + 1] = aux;
-        		 }
-        	}
-        }
-        
-        //Show all rearanged array values
-        System.out.println("\nLlistat ordenat de tots els codis entrats: ");
-        for (int i=0;i<people;i++) {
-		    System.out.println(userCode[i]);
+			//Collect data into array
+			for ( int i=0; i < people; i++ ) {
+				System.out.println("Entra el codi de l'usuari: ");
+				inRange = input.hasNextInt();
+				if(inRange) {
+					userCode[i] = input.nextInt();
+					if( userCode[i] < NUM_MIN || userCode[i] > NUM_MAX ) {
+						inRange=false;
+					}
+				}
+				if(!inRange) i--;
+				input.nextLine(); 
+			}
+				
+							
+			
+
+			//Show all array values	
+			System.out.println("\nLlistat de codis entrats: ");
+			for (int i=0;i<people;i++) {
+			    System.out.println(userCode[i]);
+			}
+
+			//bubble sort array values
+	   		 for (int i = 0; i < people - 1; i++) {
+	      		for (int j = 0; j < people - i - 1; j++) {
+	       			 if (userCode[j] > userCode[j + 1]) {
+	         		 	int aux = userCode[j];
+	        		 	userCode[j] = userCode[j + 1];
+	        		 	userCode[j + 1] = aux;
+	        		 }
+	        	}
+	        }
+	        
+	        //Show all rearanged array values
+	        System.out.println("\nLlistat ordenat de tots els codis entrats: ");
+	        for (int i=0;i<people;i++) {
+			    System.out.println(userCode[i]);
+			}
 		}
 	}
-}
