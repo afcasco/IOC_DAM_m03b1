ba36110f4980f4a26641d4fc2da780419a698d67
afcasco <afcasco@gmail.com>
2022-05-01 12:35:19 +0200

    Update Exercici_12.java

--- Exercici_12.java
+++ Exercici_12.java
@@ -1,60 +1,81 @@
-	import java.util.Scanner;
+package Unitat1;
 
-	public class Arrays {
+import java.util.Scanner;
 
-		private static final int NUM_MIN = 0;
-		private static final int NUM_MAX = 1000;
+public class E12_FernandezC {
 
-		public static void main(String[] args) {
-			
-			//variables
-			int people;
-			boolean inRange;
-			Scanner input = new Scanner(System.in);
+    private static final int NUM_MIN = 0;
+    private static final int NUM_MAX = 1000;
+    private static final String INPUT_ERROR = "Dada fora de rang! Torna a provar...";
 
-			//ask for array size and declare array
-			System.out.println("Quantes persones vols entrar: ");
-			people = input.nextInt();
-			int[] userCode = new int[people];
+    public static void main(String[] args) {
 
-			//Collect data into array
-			for ( int i=0; i < people; i++ ) {
-				System.out.println("Entra el codi de l'usuari: ");
-				inRange = input.hasNextInt();
-				if(inRange) {
-					userCode[i] = input.nextInt();
-					if( userCode[i] < NUM_MIN || userCode[i] > NUM_MAX ) {
-						inRange=false;
-					}
-				}
-				if(!inRange) i--;
-				input.nextLine(); 
-			}
-				
-							
-			
+        //variables
+        int people = 0;
+        boolean inRange;
+        boolean validSize = false;
+        Scanner input = new Scanner(System.in);
 
-			//Show all array values	
-			System.out.println("\nLlistat de codis entrats: ");
-			for (int i=0;i<people;i++) {
-			    System.out.println(userCode[i]);
-			}
+        //ask for array size, needs an integer > 0 to set validSize to true
+        System.out.println("Quantes persones vols entrar: ");
+        inRange = input.hasNextInt();
+        if (inRange) {
+            people = input.nextInt();
+            if (people > 0) {
+                validSize = true;
+            }
+        }
 
-			//bubble sort array values
-	   		 for (int i = 0; i < people - 1; i++) {
-	      		for (int j = 0; j < people - i - 1; j++) {
-	       			 if (userCode[j] > userCode[j + 1]) {
-	         		 	int aux = userCode[j];
-	        		 	userCode[j] = userCode[j + 1];
-	        		 	userCode[j + 1] = aux;
-	        		 }
-	        	}
-	        }
-	        
-	        //Show all rearanged array values
-	        System.out.println("\nLlistat ordenat de tots els codis entrats: ");
-	        for (int i=0;i<people;i++) {
-			    System.out.println(userCode[i]);
-			}
-		}
-	}
+        //If array size is valid
+        if (validSize) {
+            //decalre array with size = people
+            int[] userCode = new int[people];
+            input.nextLine();
+
+            //Collect data into array until usercode[people] is full
+            for (int i = 0; i < people; i++) {
+                inRange = false;
+                //repeat same for iteration if value is not valid (unlimited attempts given)
+                while (!inRange) {
+                    System.out.print("Entra el codi de l'usuari: ");
+                    inRange = input.hasNextInt();
+                    if (inRange) {
+                        userCode[i] = input.nextInt();
+                        if (userCode[i] < NUM_MIN || userCode[i] > NUM_MAX) {
+                            inRange = false;
+                        }
+                    }
+                    if (!inRange) System.out.println(INPUT_ERROR);
+                    input.nextLine();
+                }
+            }
+
+            //Show all array values
+            System.out.println("\nLlistat de codis entrats: ");
+            for (int i = 0; i < people; i++) {
+                System.out.println(userCode[i]);
+            }
+
+            //bubble sort array values
+            for (int i = 0; i < people - 1; i++) {
+                for (int j = 0; j < people - i - 1; j++) {
+                    if (userCode[j] > userCode[j + 1]) {
+                        int aux = userCode[j];
+                        userCode[j] = userCode[j + 1];
+                        userCode[j + 1] = aux;
+                    }
+                }
+            }
+
+            //Show all rearanged array values
+            System.out.println("\nLlistat ordenat de tots els codis entrats: ");
+            for (int i = 0; i < people; i++) {
+                System.out.println(userCode[i]);
+            }
+            //If array size is not valid
+        } else {
+            System.out.println("Error, l'array ha de tenir com a minim un element");
+        }
+    }
+}
+
