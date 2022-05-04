79465d878f1eedd5bfa7b232ed3a4f107295caa2
afcasco <afcasco@gmail.com>
2022-05-04 23:38:51 +0200

    Update Projecte1_array2.java

--- Projecte1_array2.java
+++ Projecte1_array2.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 
 public class AmpliacioP1Arrays {
 
-    //private static final int ID_MIN = 10;
-    //private static final int ID_MAX = 999;
+    /*
+    Automatically assigning incremental ids, constants not needed
+    private static final int ID_MIN = 10;
+    private static final int ID_MAX = 999;
+    */
     private static final int PLACES_MIN = 1;
     private static final int PLACES_MAX = 99;
     private static final int ROOM_MIN = 0;
@@ -15,6 +18,12 @@ public class AmpliacioP1Arrays {
     private static final int SI = 1;
     private static final int NO = 0;
     private static final int MAX_ATTEMPTS = 3;
+    private static final int END_CURRENT = -1;
+    private static final int ID_ARRAY = 0;
+    private static final int PLACES_ARRAY = 1;
+    private static final int IDIOMA_ARRAY = 2;
+    private static final int ROOM_ARRAY = 3;
+    private static final int TELF_ARRAY = 4;
     private static final String SHARED = "Habitaci compartida             ";
     private static final String SINGLE = "Habitaci                        ";
     private static final String HOME = "Habitatge sencer                 ";
@@ -22,15 +31,9 @@ public class AmpliacioP1Arrays {
     private static final String PARLA_SI = "si";
     private static final String PARLA_NO = "no";
     private static final String ERROR_MSG = "Error, input de dades no vlida";
-    private static final int END_CURRENT = -1;
-    private static final int ID_ARRAY = 0;
-    private static final int PLACES_ARRAY = 1;
-    private static final int IDIOMA_ARRAY = 2;
-    private static final int ROOM_ARRAY = 3;
-    private static final int TELF_ARRAY = 4;
-
 
     public static void main(String[] args) {
+        
         Scanner input = new Scanner(System.in);
         int attempts, nFam = 0;
         String parlaIdioma, allotjament;
@@ -49,37 +52,34 @@ public class AmpliacioP1Arrays {
             parlaIdioma = PARLA_NO;
             inRange = false;
 
-
-
- /*           System.out.println("\n(Introdueix -1 per cancelar entrada actual)");
-            while (!inRange && attempts < MAX_ATTEMPTS && !exit) {
-                System.out.println("Introdueix id (10-999):");
-                inRange = input.hasNextInt();
-                if (inRange) {
-                    families[nFam][ID_ARRAY] = input.nextInt();
-                    if (families[nFam][ID_ARRAY] < ID_MIN || families[nFam][ID_ARRAY] > ID_MAX) {
-                        inRange = false;
-                        if (families[nFam][ID_ARRAY] == END_CURRENT) {
-                            exit = true;
-                        }
-                    }
-                }
-                if (!inRange && !exit) {
-                    System.out.println(ERROR_MSG);
-                    attempts++;
-                }
-                input.nextLine();
-            }
-            if (attempts < MAX_ATTEMPTS && !exit) {
-                System.out.println("\nintrodueix -1 per cancelar entrada actual)");
-                inRange = false;
-                attempts = 0;
-            } */
+      /*           System.out.println("\n(Introdueix -1 per cancelar entrada actual)");
+                 while (!inRange && attempts < MAX_ATTEMPTS && !exit) {
+                     System.out.println("Introdueix id (10-999):");
+                     inRange = input.hasNextInt();
+                     if (inRange) {
+                         families[nFam][ID_ARRAY] = input.nextInt();
+                         if (families[nFam][ID_ARRAY] < ID_MIN || families[nFam][ID_ARRAY] > ID_MAX) {
+                             inRange = false;
+                             if (families[nFam][ID_ARRAY] == END_CURRENT) {
+                                 exit = true;
+                             }
+                         }
+                     }
+                     if (!inRange && !exit) {
+                         System.out.println(ERROR_MSG);
+                         attempts++;
+                     }
+                     input.nextLine();
+                 }
+                 if (attempts < MAX_ATTEMPTS && !exit) {
+                     System.out.println("\nintrodueix -1 per cancelar entrada actual)");
+                     inRange = false;
+                     attempts = 0;
+                 } */
 
             //automatically increase id (not user input)
-
             families[nFam][ID_ARRAY] = id;
-            System.out.println("\nId familia actual: "+id);
+            System.out.println("\nId familia actual: " + id);
 
             System.out.println("\nintrodueix -1 per cancelar entrada actual)");
             while (!inRange && attempts < MAX_ATTEMPTS && !exit) {
@@ -100,11 +100,13 @@ public class AmpliacioP1Arrays {
                 }
                 input.nextLine();
             }
+
             if (attempts < MAX_ATTEMPTS && !exit) {
                 System.out.println("\nintrodueix -1 per cancelar entrada actual)");
                 inRange = false;
                 attempts = 0;
             }
+
             while (!inRange && attempts < MAX_ATTEMPTS && !exit) {
                 System.out.println("Parla rus o ucraines? (0=no, 1=si):");
                 inRange = input.hasNextInt();
@@ -123,6 +125,8 @@ public class AmpliacioP1Arrays {
                 }
                 input.nextLine();
             }
+
+
             if (attempts < MAX_ATTEMPTS && !exit) {
                 inRange = false;
                 attempts = 0;
@@ -151,11 +155,13 @@ public class AmpliacioP1Arrays {
                 }
                 input.nextLine();
             }
+
             if (attempts < MAX_ATTEMPTS && !exit) {
                 System.out.println("\nintrodueix -1 per cancelar entrada actual)");
                 inRange = false;
                 attempts = 0;
             }
+
             while (!inRange && attempts < MAX_ATTEMPTS && !exit) {
                 System.out.println("Introdueix un telefon de contacte:");
                 inRange = input.hasNextInt();
@@ -174,6 +180,7 @@ public class AmpliacioP1Arrays {
                 }
                 input.nextLine();
             }
+
             if (attempts < MAX_ATTEMPTS && !exit) {
                 if (families[nFam][IDIOMA_ARRAY] == SI) {
                     parlaIdioma = PARLA_SI;
@@ -192,8 +199,7 @@ public class AmpliacioP1Arrays {
                         allotjament = DORM;
                         break;
                 }
-
-
+                System.out.println("Dades de la ultima familia entrada:");
                 System.out.println("Id\t\tplaces\t\trus/ucraïnès\t\ttipus\t\t\t\t\t\t\t\t\ttelèfon");
                 System.out.println(families[nFam][ID_ARRAY] + "\t\t" + families[nFam][PLACES_ARRAY] + "\t\t\t" + parlaIdioma + "\t\t\t\t\t" + allotjament + "\t\t" + families[nFam][TELF_ARRAY]);
                 nFam++;
@@ -206,8 +212,10 @@ public class AmpliacioP1Arrays {
                 System.out.println("------------------------------------------------------");
                 System.out.println("\nEntrada actual cancelada per operador");
             }
+
             inRange = false;
             attempts = 0;
+
             while (!inRange && attempts < MAX_ATTEMPTS) {
                 System.out.println("\nContinuar introduint dades? 0=no, 1=si");
                 inRange = input.hasNextInt();
@@ -223,33 +231,35 @@ public class AmpliacioP1Arrays {
                 }
                 input.nextLine();
             }
+
             if (attempts == MAX_ATTEMPTS || keepAskingInt == 0) {
                 keepAsking = false;
             }
             id++;
         } while (keepAsking);
-        System.out.println("\nS'han introduit les dades de: " + nFam + " families");
 
+        System.out.println("\nS'han introduit les seguents dades: (" + nFam + " families)");
 
-        System.out.println("Id\t\tplaces\t\trus/ucraïnès\t\ttipus\t\t\t\t\t\t\t\t\ttelèfon");
-        for (int i = 0; i < nFam; i++) {
-            if (families[i][2] == 0) {
-                parlaIdioma = PARLA_NO;
-            } else {
-                parlaIdioma = PARLA_SI;
+        if (attempts < MAX_ATTEMPTS && !exit) {
+
+            System.out.println("Id\t\tplaces\t\trus/ucraïnès\t\ttipus\t\t\t\t\t\t\t\t\ttelèfon");
+            for (int i = 0; i < nFam; i++) {
+                if (families[i][IDIOMA_ARRAY] == 0) {
+                    parlaIdioma = PARLA_NO;
+                } else {
+                    parlaIdioma = PARLA_SI;
+                }
+                if (families[i][ROOM_ARRAY] == 0) {
+                    allotjament = SHARED;
+                } else if (families[i][ROOM_ARRAY] == 1) {
+                    allotjament = SINGLE;
+                } else if (families[i][ROOM_ARRAY] == 2) {
+                    allotjament = HOME;
+                } else {
+                    allotjament = DORM;
+                }
+                System.out.println(families[i][ID_ARRAY] + "\t\t" + families[i][PLACES_ARRAY] + "\t\t\t" + parlaIdioma + "\t\t\t\t\t" + allotjament + "\t\t" + families[i][TELF_ARRAY]);
             }
-            if (families[i][3] == 0) {
-                allotjament = SHARED;
-            } else if (families[i][3] == 1) {
-                allotjament = SINGLE;
-            } else if (families[i][3] == 2) {
-                allotjament = HOME;
-            } else {
-                allotjament = DORM;
-            }
-            System.out.println(families[i][ID_ARRAY] + "\t\t" + families[i][PLACES_ARRAY] + "\t\t\t" + parlaIdioma + "\t\t\t\t\t" + allotjament + "\t\t" + families[i][TELF_ARRAY]);
         }
-
-
     }
 }
