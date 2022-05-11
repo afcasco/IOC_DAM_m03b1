6e2d107307095e19d68cbbce4e148db6d531ce26
afcasco <afcasco@gmail.com>
2022-05-11 23:16:28 +0200

    Update Exercici_14.java

--- Exercici_14.java
+++ Exercici_14.java
@@ -14,18 +14,19 @@ public class E14_FernandezC {
         boolean maleFound = false;
         boolean femaleFound = false;
 
-        int[][] dades =     {{3457,1,51,52},
-                            {3467,1,32,41},
-                            {3568,0,14,57},
-                            {3570,0,12,63},
-                            {3571,1,45,63},
-                            {3572,0,17,46},
-                            {3573,0,12,44},
-                            {3574,0,30,42},
-                            {3575,1,24,77},
-                            {3576,0,57,48}};
+        int[][] dades = {
+                {3457, 1, 51, 52},
+                {3467, 1, 32, 41},
+                {3568, 0, 14, 57},
+                {3570, 0, 12, 63},
+                {3571, 1, 45, 63},
+                {3572, 0, 17, 46},
+                {3573, 0, 12, 44},
+                {3574, 0, 30, 42},
+                {3575, 1, 24, 77},
+                {3576, 0, 57, 48}
+        };
 
-      	//show data
         for (int i = 0; i < dades.length; i++) {
             for (int j = 0; j < dades[i].length; j++) {
                 System.out.print(dades[i][j] + " ");
@@ -33,7 +34,7 @@ public class E14_FernandezC {
             System.out.println();
         }
 
-        //get first male and female row
+        //get first male and female position
         int i = 0;
         do {
             if (dades[i][1] == MALE && !maleFound) {
@@ -60,8 +61,17 @@ public class E14_FernandezC {
                 winnerF = x;
             }
         }
-        //print winners
-        System.out.println(" male WINNER in pos: " + winnerM);
-        System.out.println(" female WINNER in pos: " + winnerF);
+        //print winners with for loop
+        System.out.print("\nMale WINNER:   ");
+        for (int k = 0; k < dades[0].length; k++) {
+            System.out.print(dades[winnerM][k] + " ");
+        }
+        System.out.println();
+        System.out.print("Female WINNER: ");
+        for (int k = 0; k < dades[0].length; k++) {
+            System.out.print(dades[winnerF][k] + " ");
+        }
+        System.out.println();
     }
 }
+
